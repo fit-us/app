a54f7c65e5c97ca67779cb0d0d4a43c852bd977e
kimgho <kgh67811@naver.com>
2025-03-12 23:01:37 +0900

    style: add main color and suit font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./app.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#9D4EDD",
+      },
+      fontFamily: {
+        "suit-regular": ["SUIT-Regular"],
+        "suit-extrabold": ["SUIT-ExtraBold"],
+        "suit-bold": ["SUIT-Bold"],
+        "suit-light": ["SUIT-Light"],
+      },
+    },
   },
   plugins: [],
 };
