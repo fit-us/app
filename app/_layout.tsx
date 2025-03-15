e6a80b1a2adda3e438b44b15fb6dbc1d3a218544
kimgho <kgh67811@knu.ac.kr>
2025-03-16 03:22:40 +0900

    feature: 감정 기록을 위한 Context 작성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import "../global.css";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import "../global.css"
+import { EmotionProvider } from "@/app/EmotionContext";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    "suit-regular": require("../assets/fonts/SUIT/SUIT-Regular.ttf"),
-    "suit-extrabold": require("../assets/fonts/SUIT/SUIT-ExtraBold.ttf"),
-    "suit-bold": require("../assets/fonts/SUIT/SUIT-Bold.ttf"),
-    "suit-light": require("../assets/fonts/SUIT/SUIT-Light.ttf"),
-  });
-
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
-    }
-
     return (
-        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-            </Stack>
-            <StatusBar style="auto" />
-        </ThemeProvider>
-    );
+        <EmotionProvider>
+            <Stack
+                screenOptions={{
+                    headerShown: true,
+                    headerShadowVisible: false,
+                    headerBackTitle: "뒤로",
+                    headerBackVisible: true,
+                    headerTitle: ""
+                }}
+            />
+        </EmotionProvider>
+    )
 }
