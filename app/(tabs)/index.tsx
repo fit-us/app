bccf27c8b53e5617f761b7c42419ae308e019706
kimgho <kgh67811@knu.ac.kr>
2025-03-19 17:29:55 +0900

    chore: 목 바이오데이터로 컴포넌트 확인

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { useState, useRef } from "react"
 import EmotionSummaryContainer from "@/app/components/EmotionSummaryContainer"
 import { ImageBackground } from "expo-image"
-import { View, Text, TouchableOpacity, FlatList, Dimensions, type ViewToken, StyleSheet } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { View, Text, TouchableOpacity, FlatList, Dimensions, type ViewToken, StyleSheet, ScrollView, Platform, StatusBar } from "react-native"
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
 import { router } from "expo-router"
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import BiometricDashboard from "@/app/components/BioDashboard"
+
 
 export default function Home() {
     const emotionData = [
@@ -33,11 +34,45 @@ export default function Home() {
             dayEmotionImage: require("@/app/assets/0.png"),
         },
     ]
-
+    const biometricData = {
+        happinessScore: 85,
+        heartRate: {
+            value: 67,
+            unit: "BPM",
+            time: "오후 5:10",
+            status: "최근" as const
+        },
+        ecg: {
+            value: 72,
+            unit: "mm/mV",
+            time: "오후 5:10",
+            status: "최근" as const
+        },
+        respiratoryRate: {
+            value: 16,
+            unit: "회/분",
+            time: "오후 5:10",
+            status: "최근" as const
+        },
+        oxygenSaturation: {
+            value: 94,
+            unit: "%",
+            time: "오후 5:10",
+            status: "평균" as const
+        },
+        maxOxygenIntake: {
+            value: 49,
+            unit: "ml/분",
+            time: "오후 5:10",
+            status: "최근" as const
+        },
+    }
     const [currentIndex, setCurrentIndex] = useState<number | null>(null)
     const windowWidth = Dimensions.get("window").width
     const viewConfigRef = useRef({ viewAreaCoveragePercentThreshold: 50 })
 
+    const statusBarHeight = Platform.OS === 'ios' ? StatusBar.currentHeight || 44 : StatusBar.currentHeight || 0;
+    const insets = useSafeAreaInsets();
     const onViewableItemsChanged = useRef(({ viewableItems }: { viewableItems: ViewToken[] }) => {
         if (viewableItems.length > 0) {
             setCurrentIndex(viewableItems[0].index)
@@ -45,27 +80,32 @@ export default function Home() {
     })
     return (
         <ImageBackground source={require("@/app/assets/bgImage.png")} style={{ flex: 1 }}>
-            <SafeAreaView className="flex-1 items-center justify-center px-6">
-                <View className="flex-1 w-full">
-                    <View className="flex-row  items-center justify-between mb-4">
-                        <Text className="font-bold text-2xl px-3 text-black">3월 31일 일요일</Text>
-                        <TouchableOpacity className="mr-3 rounded-full justify-center bg-black w-16 h-9" onPress={() => router.push("/screens/form-register-emotion")}>
-                            <Text className="text-white font-semibold text-center">기록</Text>
-                        </TouchableOpacity>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ paddingBottom: insets.top }}>
+                <SafeAreaView className="flex-1 items-center justify-center px-4">
+                    <View className="flex-1">
+                        <View className="flex-row items-center justify-between mb-4">
+                            <Text className="font-bold text-2xl px-3 text-black">3월 31일 일요일</Text>
+                            <TouchableOpacity className="mr-3 rounded-full justify-center bg-black w-16 h-9" onPress={() => router.push("/screens/form-register-emotion")}>
+                                <Text className="text-white font-semibold text-center">기록</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View className="flex-1 w-full">
+                            <FlatList
+                                data={emotionData}
+                                horizontal
+                                pagingEnabled
+                                showsHorizontalScrollIndicator={false}
+                                keyExtractor={(_, index) => index.toString()}
+                                onViewableItemsChanged={onViewableItemsChanged.current}
+                                viewabilityConfig={viewConfigRef.current}
+                                renderItem={({ item }) => <EmotionSummaryContainer {...item} width={windowWidth} />}
+                            />
+                        </View>
                     </View>
-                    <FlatList
-                        data={emotionData}
-                        horizontal
-                        pagingEnabled
-                        showsHorizontalScrollIndicator={false}
-                        keyExtractor={(_, index) => index.toString()}
-                        onViewableItemsChanged={onViewableItemsChanged.current}
-                        viewabilityConfig={viewConfigRef.current}
-                        renderItem={({ item }) => <EmotionSummaryContainer {...item} width={windowWidth} />}
-                    />
-                </View>
-            </SafeAreaView>
-        </ImageBackground>
+                </SafeAreaView>
+                <BiometricDashboard data={biometricData} />
+            </ScrollView>
+        </ImageBackground >
     )
 }
 
@@ -75,5 +115,4 @@ const styles = StyleSheet.create({
         width: 20,
         backgroundColor: "#000"
     },
-
 })
